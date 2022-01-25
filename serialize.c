4113415c06dcc950f46f12953d037ad26a4880f5
charlie-map <charjhall777@gmail.com>
2022-01-25 18:02:09 -0500

    add split string helper function

--- serialize.c
+++ serialize.c
@@ -7,6 +7,67 @@
 #include "hashmap.h"
 
 /*
+	goes through a char * to create an array of char * that are split
+	based on the delimeter character. If the string was:
+
+	char *example = "Hi, I am happy, as of now, thanks";
+
+	then using split_string(example, ','); would return:
+
+	["Hi", "I am happy", "as of now", "thanks"]
+*/
+char **split_string(char *full_string, char delimeter, int *string_arr_index) {
+	int max_string_arr = 8;
+	*string_arr_index = 0;
+	char **string_arr = malloc(sizeof(char *) * max_string_arr);
+	string_arr[*string_arr_index] = malloc(sizeof(char) * 8);
+	memset(string_arr[*string_arr_index], '\0', sizeof(char) * 8);
+
+	int *string_len = malloc(sizeof(int)), curr_inner_index = 0;;
+	*string_len = 0;
+
+	// search through full string while continuing to add to the string_arr
+	// full_string should have a null terminator!
+	for (int check_full_string = 0; full_string[check_full_string]; check_full_string++) {
+		if (full_string[check_full_string] == delimeter) { // jump to next char
+			(*string_arr_index)++;
+
+			string_arr = resize_arraylist(string_arr, &max_string_arr, *string_arr_index);
+
+			string_arr[*string_arr_index] = malloc(sizeof(char) * 8);
+			memset(string_arr[*string_arr_index], '\0', sizeof(char) * 8);
+
+			curr_inner_index = 0;
+			*string_len = 8;
+
+			continue;
+		}
+
+		string_arr[*string_arr_index][curr_inner_index] = full_string[check_full_string];
+		curr_inner_index++;
+
+		// check string_arr length
+		string_arr[*string_arr_index] = resize_arraylist(string_arr[*string_arr_index], string_len, curr_inner_index);
+		string_arr[*string_arr_index][curr_inner_index] = '\0';
+	}
+
+	(*string_arr_index) += *string_arr_index != 0;
+
+	free(string_len);
+	return string_arr;
+}
+
+int destroy_split_string(char **split_string, int *split_string_len) {
+	for (int delete_sub = 0; delete_sub < *split_string_len; delete_sub++) {
+		free(split_string[delete_sub]);
+	}
+
+	free(split_string);
+
+	return 0;
+}
+
+/*
 	serialize will try to search through the void * to pull out informaiton
 	for serialization. this is challenging to define as a single "general"
 	process. however, using the char *param and function overloading the concept
@@ -40,7 +101,7 @@ int serializeMETA(void *tree, char *param, ...) {
 	FILE *index_fp = NULL;
 	FILE *title_fp = NULL;
 
-	char *format = "text/ : [id]"; // default
+	char *format = "text/ [i]: [id]"; // default
 
 	for (int check_param = 0; param[check_param]; check_param++) {
 		if (param[check_param] == 'i') {
@@ -53,6 +114,7 @@ int serializeMETA(void *tree, char *param, ...) {
 	}
 
 	// format reader to prep for the next step
+
 }
 
 int main() {
@@ -68,6 +130,17 @@ int main() {
 
 	free(total_tags);
 	free(tag);
+
+	int *split_res_length = malloc(sizeof(int));
+	*split_res_length = 0;
+	char **test_split = split_string("Hi I am happy as of now thanks", ' ', split_res_length);
+
+	for (int read_split = 0; read_split < *split_res_length; read_split++) {
+		printf("word at pos %d: %s\n", read_split, test_split[read_split]);
+	}
+
+	destroy_split_string(test_split, split_res_length);
+	free(split_res_length);
 
 	destroy_token(tree_root);
 
--- token.h
+++ token.h
@@ -9,6 +9,7 @@ token_t **token_children(token_t *parent);
 token_t *grab_token_parent(token_t *curr_token);
 
 char **token_get_tag_data(token_t *search_token, char *tag_name, int *max_tag);
+void *resize_arraylist(void *array, int *max_size, int current_index);
 
 int destroy_token(token_t *curr_token);
 
